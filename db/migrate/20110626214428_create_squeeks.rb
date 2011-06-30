cd89be22ca14c11c80fa3983b2f967fa7b478061
Ethan Stryker <e.stryker@gmail.com>
2011-06-30 22:45:59 +0100

    made a migration to change fromdatetime from time

--- db/migrate/20110626214428_create_squeeks.rb
+++ db/migrate/20110626214428_create_squeeks.rb
@@ -3,9 +3,9 @@ class CreateSqueeks < ActiveRecord::Migration
     create_table :squeeks do |t|
       t.float :latitude
       t.float :longitude
-      t.datetime :time_utc
+      t.time :time_utc
       t.string :text
-      t.datetime :expires
+      t.time :expires
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110627210906) do
+ActiveRecord::Schema.define(:version => 20110630214111) do
 
   create_table "squeeks", :force => true do |t|
     t.float    "latitude"
