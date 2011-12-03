b92ad0b8e707f612da701b698850fa314ec6dc7e
Ethan Stryker <e.stryker@gmail.com>
2011-12-03 23:09:11 +0000

    adding gemspec and docs

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,15 +8,15 @@ Rake::RDocTask.new do |rd|
   rd.options << "--inline-source"
 end
 
-#task :upload_docs => [:rdoc] do
-# sh %(scp -r html/* e_stryker@mapsqueak.rubyforge.org:/var/www/gforge-projects/mapsqueak)
-#end
+task :upload_docs => [:rdoc] do
+ sh %(scp -r html/* e_stryker@mapsqueak.rubyforge.org:/var/www/gforge-projects/mapsqueak)
+end
 
-#task :release => [:rdoc] do 
-#  sh %(gem build mapsqueak.gemspec)
-#  sh %(git push  gitosis@rubyforge.org:mapsqueak.git master)
+task :release => [:rdoc] do 
+  sh %(gem build mapsqueak.gemspec)
+  sh %(git push  gitosis@rubyforge.org:mapsqueak.git master)
   
-#  # god I love ruby:
-#  newest_gem = Dir["mapsqueak-*.gem"].sort {|a,b| File.mtime(b) <=> File.mtime(a)}.first
-#  sh "gem push #{newest_gem}"
-#end
+# god I love ruby:
+  newest_gem = Dir["mapsqueak-*.gem"].sort {|a,b| File.mtime(b) <=> File.mtime(a)}.first
+  sh "gem push #{newest_gem}"
+end
---
+++ mapsqueak.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |spec|
+  spec.name = "mapsqueak"
+  spec.version = "0.0.1"
+  spec.summary = "a REST client for MapSqueak"
+  spec.files = Dir['lib/*.rb'] + Dir['bin/*.rb']
+  spec.has_rdoc = true
+  spec.bindir = 'bin'
+  spec.author = "Ethan Stryker"
+  spec.email = "e.stryker@gmail.com"
+  spec.rubyforge_project = "mapsqueak"
+  spec.description = <<-END
+  A REST client for MapSqueak, a mobile enabled web app. 
+  END
+  spec.homepage = 'http://mapsqueak.rubyforge.org/'
+end
