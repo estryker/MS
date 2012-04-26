54ced85beece0d17b293108ef1cc88f8e76cebf7
Ethan Stryker <e.stryker@gmail.com>
2012-04-26 02:59:11 +0100

    facebook link change

--- app/controllers/share_requests_controller.rb
+++ app/controllers/share_requests_controller.rb
@@ -108,12 +108,12 @@ picture_url = "http://bit.ly/IefF8a"
       begin 
       # Use google's static map api to get an image for the squeak
       id = user.put_wall_post("I just posted to MapSqueak!",
-      { # :name => squeak.text,
+      { :name => squeak.text,
         # TODO: this is a Rack app, so get its current host
         :link => "http://mapsqueak.heroku.com/squeaks/#{squeak.id}",
-        # :caption => Time.now < squeak.expires ? "Expires in #{time_ago_in_words(squeak.expires)}" : "Expired #{time_ago_in_words(squeak.expires)} ago."
+        :caption => Time.now < squeak.expires ? "Expires in #{time_ago_in_words(squeak.expires)}" : "Expired #{time_ago_in_words(squeak.expires)} ago.",
         # :description => "the description of the squeak, TBD",
-        # => picture_url
+        :image => picture_url
         })
      rescue Exception => e
         flash[:error] = "Error: couldn't post to facebook wall"
