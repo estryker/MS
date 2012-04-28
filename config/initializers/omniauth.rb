ac69a2b3121292ce21711b8dbf8aeca7e8173b0c
Ethan Stryker <e.stryker@gmail.com>
2012-04-28 12:15:19 -0400

    added publish_stream scope to twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,  '107582139349630', 'ca16bbd5834ab7d4b012ec5e84a0d003', :scope => "publish_stream"
-  provider :twitter, 'K1tkT7Jpi3Ujl0Ftv2V1A', 'UzXlol9ZoDd5uJzuhJpiEFtT0reBcQdTO8XSLVp1k'
+  provider :twitter, 'K1tkT7Jpi3Ujl0Ftv2V1A', 'UzXlol9ZoDd5uJzuhJpiEFtT0reBcQdTO8XSLVp1k', :scope => "publish_stream"
   # provider :google, 'domain.com', 'secret', :scope => 'https://mail.google.com/mail/feed/atom/'"
   # provider :google_oauth2, 'mapsqueakomni.heroku.com', :scope => 'https://www.googleapis.com/auth/plus.me'
 end
