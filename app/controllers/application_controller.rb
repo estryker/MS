9223b1f21b0ba15529e78c382ebc4ed3c65f15dc
Ethan Stryker <e.stryker@gmail.com>
2012-05-02 03:12:56 +0100

    moved Message class to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include SessionsHelper
+  include SessionsHelper 
+  
+  # Messages are just a loose wrapper around XML or JSON blobs used for error messages or success messages when 
+  # there are no data types to be returned. 
+  class Message
+    def initialize(text,code)
+      @info = {:text => text, :code => code.to_i}
+    end
+
+    def to_xml
+      @info.to_xml(:root => 'message')
+    end
+
+    def to_json
+      @info.to_xml
+    end
+
+    def text
+      @info[:text]
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,23 +16,5 @@ module ApplicationHelper
     image_tag("mapsqueak_name.png", :alt => "Mapsqueak", :class => "round")
   end
 
-  # Messages are just a loose wrapper around XML or JSON blobs used for error messages or success messages when 
-  # there are no data types to be returned. 
-  class Message
-    def initialize(text,code)
-      @info = {:text => text, :code => code.to_i}
-    end
-
-    def to_xml
-      @info.to_xml(:root => 'message')
-    end
-
-    def to_json
-      @info.to_xml
-    end
-
-    def text
-      @info[:text]
-    end
-  end
+ 
 end
