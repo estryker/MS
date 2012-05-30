968017a47351dc03818784ed8fe08b405cb9003a
Ethan Stryker <e.stryker@gmail.com>
2012-05-30 02:56:36 +0100

    fixed gravatar_image for twitter users with no email

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,7 @@
 module UsersHelper
   def gravatar_for(user, options = { :size => 50 })
+    email = user.email
+    email.downcase! unless email.nil?
     gravatar_image_tag(user.email.downcase, :alt => user.name,
                                             :class => 'gravatar',
                                             :gravatar => options)
