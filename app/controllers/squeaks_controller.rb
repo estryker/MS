4d5a999f593a7c46a9e4ae01a9253f6896a7d817
Ethan Stryker <e.stryker@gmail.com>
2012-07-06 11:07:26 -0400

    updated hmac checking to trim received

--- app/controllers/squeaks_controller.rb
+++ app/controllers/squeaks_controller.rb
@@ -24,7 +24,7 @@ class SqueaksController < ApplicationController
     if params[:squeak].has_key? :salt and params[:squeak].has_key? :hash
       key = "OIA9cj6nTfiV4EHkfDZc2A" # test
       hmac = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('md5'),key,params[:squeak][:salt]))
-      if hmac == params[:squeak][:hash].trim
+      if hmac == params[:squeak][:hash].strip
         puts "HMAC correct"
       else
         puts "No HMAC match: \'#{hmac}\' vs received: \'#{params[:squeak][:hash]}\'"
