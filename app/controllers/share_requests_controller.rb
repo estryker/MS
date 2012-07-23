87141b6797469ad58e9b0d1aad953e058ad70aa3
Ethan Stryker <e.stryker@gmail.com>
2012-07-22 21:53:04 -0400

    cleaned up facebook sharing debug code

--- app/controllers/share_requests_controller.rb
+++ app/controllers/share_requests_controller.rb
@@ -11,57 +11,12 @@ class ShareRequestsController < ApplicationController
     # we expect params to have :squeak_id and :provider. We will determine the :user_id of the requester
     squeak = Squeak.find(params[:squeak_id])
 
-    # debug
-    if(false) # root_url.include?('localhost'))
-       token = 'AAABh2GszOn4BAHw3dZCJZBSDJTsR117vdDuJmLOUcSdzuKo4qyZBaOtQZB6dvDZC732ZAxkBsvMuWLoVlcvFgzdhSXB8FpPmM6CcvcvAw36gZDZD'
-       uid = '1456286987'
-       caption = Time.now < squeak.expires ? "Expires in #{time_ago_in_words(squeak.expires)}" : "Expired #{time_ago_in_words(squeak.expires)} ago."
-       uri = URI("https://graph.facebook.com/")
-       https = Net::HTTP.start(uri.host, uri.port,:use_ssl => true)
-       #Net::HTTP.post_form(,"access_token"=>token, "message" => "I just posted to MapSqueak!",
-       #                   "link" => "http://mapsqueak.heroku.com/squeaks/#{squeak.id}&caption=#{caption}")
-       
-       escape = URI.escape("message=I just posted to MapSqueak!&access_token=#{token}&link=http://mapsqueak.heroku.com/squeaks/#{squeak.id}&caption=#{caption}&picture=#{squeak_map_preview(squeak)}")
-       response = https.post("/#{uid}/feed",escape)
-       puts "attempted to share to fb: #{response.body}"
-       
-       redirect_to index_path
-       return
-      
-    elsif(root_url.include?('localhost'))
-      user = Koala::Facebook::API.new('AAABh2GszOn4BAHw3dZCJZBSDJTsR117vdDuJmLOUcSdzuKo4qyZBaOtQZB6dvDZC732ZAxkBsvMuWLoVlcvFgzdhSXB8FpPmM6CcvcvAw36gZDZD')
-      caption = Time.now < squeak.expires ? "Expires in #{time_ago_in_words(squeak.expires)}" : "Expired #{time_ago_in_words(squeak.expires)} ago."
-      
-      #`curl -F 'access_token=#{auth.token}' -F 'message=I just posted to MapSqueak!' -F 'link=http://mapsqueak.heroku.com/squeaks/#{squeak.id}' -F 'caption=#{caption} https://graph.facebook.com/#{auth.uid}/feed`
-      facebook_args = { 
-        :description => "MapSqueak. What\'s happening around you, right now?",
-        :link => "http://www.mapsqueak.com/squeak.php?id=#{squeak.id}", # :link => "#{root_url}squeaks/#{squeak.id}", # 
-        :name => squeak.text,
-        :caption => caption
-      }
-
-      ret = user.put_wall_post(squeak.text, facebook_args)
-      redirect_to index_path
-      return
-    end
-
+  
     if squeak.nil?
       # throw an appropriate error and redirect to the index_path
       respond_to_user("Can't find squeak with id of #{params[:squeak_id]}",1,index_path)
     else
-      if(false)
-        # debug only for local testing!!!
-        debug = Koala::Facebook::API.new("AAABh2GszOn4BAHw3dZCJZBSDJTsR117vdDuJmLOUcSdzuKo4qyZBaOtQZB6dvDZC732ZAxkBsvMuWLoVlcvFgzdhSXB8FpPmM6CcvcvAw36gZDZD")
-        debug.put_wall_post('I just posted to MapSqueak!', { :name => squeak.text,
-                              :description => "I just posted to MapSqueak!",
-                              :link => 'www.istherea.com',# squeak_link,
-                              :caption => Time.now < squeak.expires ? "Expires in #{time_ago_in_words(squeak.expires)}" : "Expired #{time_ago_in_words(squeak.expires)} ago.",
-                              # :description => "Posted on MapSqueak!" ,
-                              :picture => squeak_map_preview(squeak)
-                              
-                            })
-      end
-
+     
       if signed_in_to?(params[:provider])
         #request = ShareRequest.new(params.merge({:user_id => current_user.id}))
         share_request = ShareRequest.new({:user_id => current_user.id,:squeak_id => params[:squeak_id],:provider=>params[:provider]})
@@ -176,43 +131,17 @@ class ShareRequestsController < ApplicationController
             :caption => caption
           }
 
-          # This should work, but it doesn't anymore. reverting to link
-          #if squeak.image.nil?
-          #  facebook_args.merge!(:image => "#{root_url}squeaks/image/#{squeak.id}")
-          #else  
-          #  # **Note that 'picture' is what is documented. Image was found by mistake. 
-          #  # facebook_args.merge!(:picture => "#{root_url}/images/mapsqueak_logo.png)")
-          #  facebook_args.merge!(:image => "#{root_url}squeaks/map_image/#{squeak.id}")
-          #end
-
           if squeak.image.nil?
             # facebook_args.merge! :picture => "#{root_url}squeaks/map_image/#{squeak.id}.jpg"
           else
             facebook_args.merge! :picture => "#{root_url}squeaks/image/#{squeak.id}.jpg"
           end
 
-          # facebook_args.merge! :picture => "#{root_url}squeaks/map_image/#{squeak.id}.jpg"
-          # facebook_args.merge! :picture => "http://mapsqueak.com/images/mapsqueak.png"
-          
-          # this works, but image doesn't show up right when posted from heroku
           ret = user.put_wall_post(squeak.text, facebook_args)
-          
-          # user.get_object(ret["id"]) # to ensure the image gets shared ...
 
-          # don't need these in rails:
-          # require 'uri'
-          # require 'net/http'
-          #uri = URI("https://graph.facebook.com/")
-          #https = Net::HTTP.start(uri.host, uri.port,:use_ssl => true)
-          #Net::HTTP.post_form(,"access_token"=>token, "message" => "I just posted to MapSqueak!",
-          #                   "link" => "http://mapsqueak.heroku.com/squeaks/#{squeak.id}&caption=#{caption}")
-          
-          #escape = URI.escape("message=#{squeak.text}&access_token=#{auth.token}&link=http://www.mapsqueak.com/squeak.php?id=#{squeak.id}&caption=#{caption}")
-          #response = https.post("/#{uid}/feed",escape)
-          #puts "attempted to share to fb: #{response.body}"
-          #puts "Updated facebook: #{ret.inspect}"
-          
-        rescue Exception => e
+          # user.get_object(ret["id"]) # to ensure the image gets pulled in?
+
+         rescue Exception => e
           # flash[:error] = "Error: couldn't post to facebook wall"
           puts "Error posting squeak:"
           puts e.message
