6427bcd3ae793f6c391f47cfe0036126c3613335
Ethan Stryker <e.stryker@gmail.com>
2012-08-31 21:49:37 -0400

    starting to add share_request tests

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ group :test do
   gem 'rspec-rails', '2.6.1'
   gem 'webrat', '0.7.1'
   gem 'factory_girl_rails', '1.0'
+  gem 'vcr'
 end
 
 group :production do 
--- spec/controllers/share_request_controller_spec.rb
+++ spec/controllers/share_request_controller_spec.rb
@@ -8,5 +8,38 @@ describe ShareRequestsController do
   #    response.should be_success
   #  end
   #end
+  describe "POST create to facebook" do 
+    before(:each) do
+      @squeak = Factory(:squeak) 
+      @user = Factory(:user)
+      test_sign_in(@user)   
+      
+      # need to get an oath token somehow ...
+      test_users = Koala::Facebook::TestUsers.new(:app_id => '107582139349630', :secret => "ca16bbd5834ab7d4b012ec5e84a0d003")
+      user_info = test_users.create(true, "offline_access,read_stream,manage_pages,publish_stream")
+      login_url = user_info['login_url']
+      user = Koala::Facebook::API.new(user_info['access_token'])
+      
+      auth = OmniAuth.config.mock_auth[:facebook]
+      auth[:credentials][:token] = user_info['access_token']
 
+      request.env["omniauth.auth"] = auth
+      post 'share_request/create' # how do I post to another controller??
+
+      # Configure Koala for VCR
+      Koala.http_service.faraday_middleware = Proc.new do |builder|
+        builder.use VCR::Middleware::Faraday do |cassette|
+          cassette.name    'facebook'
+          cassette.options :record => :new_episodes
+        end
+        builder.use Koala::MultipartRequest
+        builder.request :url_encoded
+        builder.adapter Faraday.default_adapter
+      end
+    end
+    it "should be successful" do 
+      post 'create', :squeak_id => @squeak.id, :provider => 'facebook'
+      response.should be_success, "Response: #{response.body}"
+    end
+  end
 end
