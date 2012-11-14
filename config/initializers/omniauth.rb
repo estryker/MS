7cb975ef6712943ddf16bff7d26017283156f693
Ethan Stryker <e.stryker@gmail.com>
2012-11-13 23:32:16 -0500

    updated facebook secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Note I couldn't find publish type settings on the facebook developer page
-  provider :facebook,  '107582139349630', 'ca16bbd5834ab7d4b012ec5e84a0d003', :scope => "publish_stream,publish_checkins" # read_stream
+  # old ca16bbd5834ab7d4b012ec5e84a0d003'
+  provider :facebook,  '107582139349630', '25da8dc7ba1ee69eba5fc2c316ea6528' , :scope => "publish_stream,publish_checkins" # read_stream
 
   # Note that you can change an app's permissions on the twitter development page
   provider :twitter, 'K1tkT7Jpi3Ujl0Ftv2V1A', 'UzXlol9ZoDd5uJzuhJpiEFtT0reBcQdTO8XSLVp1k'
