49981deea0b28bba92e3e0d20ca1f88d8d239249
Ethan Stryker <e.stryker@gmail.com>
2012-12-26 20:53:05 -0500

    moved squeak duration custom validator to app/validators

---
+++ app/validators/squeak_validators.rb
@@ -0,0 +1,25 @@
+class SqueakDurationValidator < ActiveModel::EachValidator
+  def validate_each(squeak_record, attribute, value)
+    if value > 0
+      if squeak_record.user_id 
+        user = User.find(squeak_record.user_id) 
+        if user && user.role_id
+          role = Role.find(user.role_id) 
+          unless role && value <= (max_duration = role.max_squeak_duration || 24)
+            squeak_record.errors.add attribute , "duration must be less than #{max_duration} according to current role"
+          end
+        else
+          unless value <= 24
+            squeak_record.errors.add attribute, "duration must be less than 24 hours by default"
+          end
+        end
+      else 
+        unless value <= 24
+          squeak_record.errors.add attribute, "duration must be less than 24 for squeaks with no user id"
+        end
+      end
+    else
+      squeak_record.errors.add attribute, "duration must be greater than 0"
+    end
+  end
+end
